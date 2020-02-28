0c83935a3e4c5d70e502721b80bd5ce1e69a4e7c
thedannysoto <87dsoto@gmail.com>
2020-02-28 14:38:47 -0600

    created add new game form

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,3 +1,11 @@
 class GameController < ApplicationController 
-    
+
+
+    get '/games' do 
+        erb :'/games/games'
+    end
+
+    get '/games/new' do 
+        erb :'/games/new'
+    end
 end
