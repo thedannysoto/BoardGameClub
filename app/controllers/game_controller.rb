2e2b8da6ed2acccf595ac7c2e78d18bb31a2f4c6
thedannysoto <87dsoto@gmail.com>
2020-02-28 15:32:49 -0600

    add new game form instantiates new game object

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,4 +8,11 @@ class GameController < ApplicationController
     get '/games/new' do 
         erb :'/games/new'
     end
+
+    post '/games' do 
+        #Update: raise error if Game has no name
+        game = Game.new(:name => params[:name])
+        game.save
+        redirect '/games'
+    end
 end
