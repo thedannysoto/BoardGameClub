c32aa2ba3524c9d820e65ecba09ecc91f6f3dac3
thedannysoto <87dsoto@gmail.com>
2020-02-28 10:09:59 -0600

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+require './config/environment'
+require 'securerandom'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+end
