d120ff516975efb71145f63da117a141b017e86e
thedannysoto <87dsoto@gmail.com>
2020-02-28 11:34:48 -0600

    adds has_many relationships to user and game models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base 
-    
+    has_many :user_games
+    has_many :users, through: :user_games
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base 
-    has_one :collection
     has_many :user_games 
     has_many :games, through: :user_games
 end
